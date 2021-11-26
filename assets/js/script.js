98cb360aa87319c026dced5f71adbefa30c05364
CKURZ <ckurz098@gmail.com>
2021-11-26 12:50:07 -0800

    created start button that inits quiz and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,64 @@
-function drawStartCard() {
+document.getElementById("quizCard").style.display="none";
+document.getElementById("finishCard").style.display="none";
 
+function initQuestions(){
+    array=[
+        {
+            question: "Here is question 1",
+            code: "code Example",
+            choiceA: "FirstChoice",
+            choiceB: "SecondChoice",
+            choiceC: "ThirdChoice",
+            choiceD: "FourthChoice",
+            correct : "B"
+        },
+        {
+            question: "Here is question 2",
+            code: "code Example",
+            choiceA: "FirstChoice",
+            choiceB: "SecondChoice",
+            choiceC: "ThirdChoice",
+            choiceD: "FourthChoice",
+            correct: "D"
+        },
+    ]
+    startQuiz(array);
 }
+function startQuiz(questionArray) {
+    //hide startCard element
+    document.getElementById("startCard").style.display="none";
+    document.getElementById("finishCard").style.display="none";
+    document.getElementById("quizCard").style.display="block";
+    
+    let time = 10;
+    let timerDisplay = document.getElementById("timerDisplay");
+    timerDisplay.textContent = "Time: " + time;
 
-function drawQuestionCard() {
-    //create elements
-
-
-    let time = 120;
     let timer = setInterval(function () {
         time--;
-        document.getElementById("timerDisplay").textContent = "Time: " + time;
-        if (timer <= 0) {
-            drawFinishCard(false);
+        timerDisplay.textContent = "Time: " + time;
+        
+        if (time <= 0) {
             clearInterval(timer);
+            drawFinishCard(false);
         }
+
     }, 1000);
 
+    getQuestion(questionArray);
+}
+///////////////Displays the questions and choices and waits for input
+function getQuestion(array){
+    questionNumber =0;
 
 }
 
 function drawFinishCard(completed) {
-
+    document.getElementById("startCard").style.display="none";
+    document.getElementById("quizCard").style.display="none";
+    document.getElementById("finishCard").style.display="block";
 }
 
 
-function clearCard() {
-    //clears all elements by removing surrounding div
-}
+document.getElementById("startButton").addEventListener("click",initQuestions);
 
-
-
-drawQuestionCard();
